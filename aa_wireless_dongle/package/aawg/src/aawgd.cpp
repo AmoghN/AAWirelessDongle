8bea699188456ded279b8f472644cdaa0c05554e
Amogh Nayak <amoghnk03@gmail.com>
2023-12-24 21:47:41 -0500

    reverted order in aawgd

--- aa_wireless_dongle/package/aawg/src/aawgd.cpp
+++ aa_wireless_dongle/package/aawg/src/aawgd.cpp
@@ -16,6 +16,9 @@ int main(void) {
     BluetoothHandler::instance().init();
 
     while (true) {
+        // Per connection setup and processing
+        UsbManager::instance().enableDefaultAndWaitForAccessroy();
+
         AAWProxy proxy;
         std::optional<std::thread> proxyThread = proxy.startServer(Config::instance()->getWifiInfo().port);
 
@@ -25,10 +28,7 @@ int main(void) {
 
         BluetoothHandler::instance().connect();
 
-        // Per connection setup and processing
-        UsbManager::instance().enableDefaultAndWaitForAccessroy();
         proxyThread->join();
-        
         BluetoothHandler::instance().powerOff();
         UsbManager::instance().disableGadget();
 
